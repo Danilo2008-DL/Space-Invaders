7f789dc9858c7f02c93b81288f0c17721b005df4
Danilo2008-DL <63961886+Danilo2008-DL@users.noreply.github.com>
2020-04-21 00:38:23 -0300

    Mudança na parte de fundo

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ pygame.init()
 x = 400
 y = 300
 velocidade = 35
-fundo = pygame.image.load("images/iv.png")
+fundo = pygame.image.load("images/Mapa do jogo.png")
 nave = pygame.image.load("images/battleship.png")
 
+
 janela = pygame.display.set_mode((800,600))
-pygame.display.set_caption("Game")
+pygame.display.set_caption("Space Invaders")
 
 janela_aberta = True
 while janela_aberta :
@@ -28,12 +29,25 @@ while janela_aberta :
         x += velocidade
     janela.fill((0,0,0))
 
-
     janela.blit(fundo,(0,0))
     janela.blit(nave, (x,y))
 
     pygame.draw.circle(janela,(0,0,0),(x,y),0)
     pygame.display.update()
+
+    def __init__(self, x, y):
+        auto. x = x
+        auto. y = y
+        auto. d = 10
+        auto. velocidade = -5
+
+    def draw(self):
+        pygame. desenhar. elipse(display, laranja,(self. x, eu. y, self. d, self. d))
+
+    def move(self):
+        auto. y += self. velocidade
+
+
 
 pygame.quit()
 
